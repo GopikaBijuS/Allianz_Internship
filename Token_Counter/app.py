b0286e8b2b24b283ead9a4df15fde6601774721e
Gopika Biju S <gopikabijus20@gmail.com>
2026-06-01 02:58:28 +0530

    File read and token count is done

--- Token_Counter/app.py
+++ Token_Counter/app.py
@@ -1,1 +1,20 @@
-print("This is the Token Counter App")
+import sys
+import tiktoken
+
+try:
+    filename=sys.argv[1]
+    with open(filename,"r") as file:
+        text=file.read()
+
+    encoding=tiktoken.get_encoding("cl100k_base")
+
+    tokens=encoding.encode(text)
+    print("Tokens: ",tokens)
+
+    token_count=len(tokens)
+    print("Token Count: ",token_count)
+
+except FileNotFoundError:
+    print("File not found")
+except IndexError:
+    print("Please provide a filename as an argument")
